e196d992318fa7a9ebb6827d0d5dca6cfacb1e0a
袁振圃 <390179298@qq.com>
2019-11-12 10:35:30 +0800

    Revert "Revert "no message""
    This reverts commit b2048de7d8bcca7a96c029802bb96659b315ed5a.

---
+++ js/vue-scroll.js
@@ -0,0 +1,19 @@
+	var w = new Vue({
+		el: "#wrapper",
+		data: {
+			//表示显示的图标
+			static: "",
+			lis: [
+				{name:"一"},
+				{name:"二"},
+				{name:"三"},
+				{name:"四"},
+				{name:"五"},
+				{name:"六"},
+				{name:"七"},
+				{name:"八"},
+				{name:"九"},
+				{name:"十"},
+			]
+		}
+	})
